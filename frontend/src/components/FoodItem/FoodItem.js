523817bb9ce9dffe43d328372c1381e2ea5dc3db
Kalhara2000 <20.kalhara@gmail.com>
2024-07-06 15:04:08 +0530

    Responsive & Required
    Responsive : footer, news card, mobile-app banner
    
    Required: All input fields in PlaceOder.js ( but first name field not required)

--- frontend/src/components/FoodItem/FoodItem.js
+++ frontend/src/components/FoodItem/FoodItem.js
@@ -1,34 +1,45 @@
-import React, { useContext} from 'react';
-import './FoodItem.css';
-import { assets } from '../../asset/assets';
-import {StoreContext} from '../../context/StoreContext'
+import React, { useContext } from "react";
+import "./FoodItem.css";
+import { assets } from "../../asset/assets";
+import { StoreContext } from "../../context/StoreContext";
 
-
-const FoodItem = ({ id, name, price, description, image }) => { // Correct spelling
-
-const {cartItems,addToCart,removeFromCart} = useContext(StoreContext) 
+const FoodItem = ({ id, name, price, description, image }) => {
+  const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
 
   return (
-    <div className='food-item'>
+    <div className="food-item">
       <div className="food-item-img-container">
-        <img className='food-item-image' src={image} alt={name} />
-        {!cartItems[id]
-           ?<img className='add' onClick={()=>addToCart(id)} src={assets.add_icon_white }/>
-             :<div className='food-item-counter'>
-              <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt=""/> 
-              <p>{cartItems[id]}</p>
-              <img onClick={()=>addToCart(id)} src={assets.add_icon_green} alt=''/>
-
-               </div>
-        }
+        <img className="food-item-image" src={image} alt={name} />
+        {!cartItems[id] ? (
+          <img
+            className="add"
+            onClick={() => addToCart(id)}
+            src={assets.add_icon_white}
+            alt="Add to cart"
+          />
+        ) : (
+          <div className="food-item-counter">
+            <img
+              onClick={() => removeFromCart(id)}
+              src={assets.remove_icon_red}
+              alt="Remove from cart"
+            />
+            <p>{cartItems[id]}</p>
+            <img
+              onClick={() => addToCart(id)}
+              src={assets.add_icon_green}
+              alt="Add more to cart"
+            />
+          </div>
+        )}
       </div>
       <div className="food-item-info">
         <div className="food-item-name-rating">
           <p>{name}</p>
-          <img src={assets.rating_starts} alt="rating stars" />
+          <img src={assets.rating_starts} alt="Rating stars" />
         </div>
-        <p className='food-item-desc'>{description}</p> {/* Correct spelling */}
-        <p className='food-item-price'>Rs: {price}</p>
+        <p className="food-item-desc">{description}</p>
+        <p className="food-item-price">Rs: {price}</p>
       </div>
     </div>
   );
