7fe2c2f298a805a3a30b70e23b31129e00272890
Kalhara2000 <20.kalhara@gmail.com>
2025-06-16 23:19:22 +0530

    changes menu titles

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      "mongodb+srv://KalharaAlgo:GREMCCkIxhYiNNIP@kalhara00.igsiuzf.mongodb.net/food-del"
+      "mongodb+srv://KalharaAlgo:YemhVIzhBBoU7QnI@kalhara00.igsiuzf.mongodb.net/food-del"
     )
     .then(() => console.log("DB connected"));
 };
--- frontend/src/asset/assets.js
+++ frontend/src/asset/assets.js
@@ -109,21 +109,21 @@ export const assets = {
 
 export const menu_list = [
     {
-        menu_name: "Salad",
+        menu_name: "Fruit Salad",
         menu_image: menu_1
     },
     {
-        menu_name: "Rolls",
+        menu_name: "Fruit Juice",
         menu_image: menu_2
     },
-    {
-        menu_name: "Deserts",
-        menu_image: menu_3
-    },
-    {
-        menu_name: "Sandwich",
-        menu_image: menu_4
-    },
+    // {
+    //     menu_name: "Deserts",
+    //     menu_image: menu_3
+    // },
+    // {
+    //     menu_name: "Sandwich",
+    //     menu_image: menu_4
+    // },
     {
         menu_name: "Cake",
         menu_image: menu_5
@@ -133,11 +133,11 @@ export const menu_list = [
         menu_image: menu_6
     },
     {
-        menu_name: "Pasta",
+        menu_name: "Fruit Jams",
         menu_image: menu_7
     },
     {
-        menu_name: "Noodles",
+        menu_name: "Pure Fruits",
         menu_image: menu_8
     }
 ]
